3221feae10c3429da2a934b9c656153c83c2fe5f
João Vinicius Rodrigues do Nascimento <joaoviniciusrodrigues@users.noreply.github.com>
2020-07-29 23:30:14 -0300

    Melhorias no formulario de clientes

--- frontend/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ frontend/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -10,9 +10,15 @@ export class ClientesFormComponent implements OnInit {
 
   cliente : Cliente;
 
-  constructor() { }
+  constructor() { 
+    this.cliente = new Cliente();
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log(this.cliente);
   }
 
 }
